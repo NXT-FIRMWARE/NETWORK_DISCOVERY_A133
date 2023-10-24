e6a46a7377131bfc8599408b484fb3f372aa18d7
Brayer <116017802+NextBrayer@users.noreply.github.com>
2023-10-24 12:09:10 +0100

    Update client.py

--- utils/client.py
+++ utils/client.py
@@ -1,11 +1,18 @@
 import socket
 import sys
 import struct
+import netifaces
+import json
 
 class Client:
     def __init__(self,multicast_address,port,message):
         print("[i] init ...")
-        self.message = message
+        data = {
+           "hostname" : socket.gethostname(),
+           "wifi_mac" : netifaces.ifaddresses('wlan0')[netifaces.AF_LINK][0]['addr'],
+           "ethernet_mac" : netifaces.ifaddresses('eth0')[netifaces.AF_LINK][0]['addr'],
+        }
+        self.message = json.dumps(data)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -20,7 +27,7 @@ class Client:
                 data, addr = self.sock.recvfrom(1024)
                 print("[i] signal received from server")
                 print("[i] send message to server")
-                self.sock.sendto(bytes(self.message,'utf-8'),addr)
+                self.sock.sendto(bytes(self.message,"utf-8"),addr)
         except KeyboardInterrupt:
             print('[d] finish Executing ... ')
             sys.exit("[d] exit code")
